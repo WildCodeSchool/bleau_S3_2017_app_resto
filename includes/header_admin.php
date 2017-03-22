61d55e90aeeac87eba28c71dc6e8a5a4e9e0fedc
SebiLob <solomon.grundy.51@gmail.com>
2017-03-22 10:29:58 +0100

    save

--- includes/header_admin.php
+++ includes/header_admin.php
@@ -2,14 +2,14 @@
 
 	<header>
 		<div class="row head">
-			<div class="col s4">
+			<div class="col s4 logo">
 				<img src="img/logo_newrest.png">
 			</div>
 			<div class="col s4 center-align">
 				<h1 class="h1">APP RESTO</h1>
 			</div>
 			<div class="col s4 right-align">
-				<a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Aller vers la page Cantine">Page Cantine</a>
+				<a class="btn tooltipped boutton" data-position="bottom" data-delay="50" data-tooltip="Aller vers la page Cantine">Page Cantine</a>
 			</div>
 		</div>
 	</header>
--- includes/header_index.php
+++ includes/header_index.php
@@ -5,14 +5,14 @@ mettre l'include du fichier header_index.php avat la class container
 
 	<header>
 		<div class="row head">
-			<div class="col s4">
+			<div class="col s4 logo">
 				<img src="img/logo_newrest.png">
 			</div>
 			<div class="col s4 center-align">
 				<h1 class="h1">APP RESTO</h1>
 			</div>
 			<div class="col s4 right-align">
-				<a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Voir les profil des employés">A Propos</a>
+				<a class="btn tooltipped boutton" data-position="bottom" data-delay="50" data-tooltip="Voir les profils des employés">A Propos</a>
 			</div>
 		</div>
 	</header>
