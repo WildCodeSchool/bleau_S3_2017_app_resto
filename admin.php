dd8fbc86b882b935b8f046f45641f8a170aea2e0
coco-b <ccl.berger@gmail.com>
2017-03-28 14:21:12 +0200

    modification front

--- admin.php
+++ admin.php
@@ -7,6 +7,8 @@
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
 
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
   <link rel="stylesheet" type="text/css" href="css/admin.css"/>
 
   <title>Page_Admin</title>
