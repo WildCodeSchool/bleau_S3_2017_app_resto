3907f960e602ad09ac77bd4ba9526e32135217ba
Cecile_Berger <ccl.berger@gmail.com>
2017-04-05 17:48:07 +0200

    fix template admin

--- src/AppRestoBundle/Controller/AdminController.php
+++ src/AppRestoBundle/Controller/AdminController.php
@@ -8,7 +8,14 @@ class AdminController extends Controller
 {
     public function adminAction()
     {
-        return $this->render('AppRestoBundle:Admin:admin.html.twig');
+
+        $days = ['','Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
+        $types = ['Plat', 'Garniture', 'Entrée', 'Dessert'];
+
+        return $this->render('AppRestoBundle:Admin:admin.html.twig',
+            array ('days' => $days, 'types' => $types )
+            );
+
     }
 
     public function counterAction()
